5440e4c1d752fbb04e3c35c626dfb2b2fcf6d20f
Shivam Pandey <shivampandey88@hotmail.com>
2024-04-23 20:49:25 +0530

    Completed admin portal

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px", // Small screens, like mobile
+      md: "768px", // Medium screens, like tablets
+      lg: "1024px", // Large screens, like laptops
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "#FFFFFF",
+        buttonColor: "#063970",
+        inputBoxColor: "#E5E5E5",
       },
     },
   },
