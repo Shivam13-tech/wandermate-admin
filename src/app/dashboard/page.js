8adfe909499451f5fcc4a1704b1c5ccb2b155bdb
Shivam Pandey <shivampandey88@hotmail.com>
2024-06-25 23:28:10 +0530

    Completed admin panel content

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
-import Image from "next/image";
-import { useState } from "react";
+
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Dashboard() {
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [showContent, setShowContent] = useState({});
 
   const toggleVisibility = (index) => {
@@ -13,51 +14,159 @@ export default function Dashboard() {
     }));
   };
 
-  const trekkingExpeditions = [
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-    "Embark on a journey through the untamed beauty of the Himalayas with our exhilarating trekking expedition. This unforgettable adventure begins at the base of towering peaks, where you will find yourself surrounded by lush forests, cascading waterfalls, and breathtaking vistas at every turn.",
-  ];
+  const [guidetoken, setGuideToken] = useState("");
+  useEffect(() => {
+    const GuideToken = localStorage.getItem("Guidetoken");
+    if (GuideToken) {
+      setGuideToken(GuideToken);
+    }
+  }, []);
 
-  const images = ["/Images/1.png", "/Images/2.png", "/Images/3.png"];
+  const [tours, setTours] = useState([]);
+  useEffect(() => {
+    const fetchTourData = async () => {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:8080/api/tour/gettour",
+          {
+            headers: {
+              Authorization: `Bearer ${guidetoken}`,
+            },
+          }
+        );
+        console.log(response.data);
+        setTours(response.data.Result);
+      } catch (error) {
+        console.error("Failed to fetch tours:", error);
+      }
+    };
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
+    fetchTourData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [guidetoken]);
+
+  const [tourdetail, setTourDetail] = useState({
+    name: "",
+    price: "",
+    description: "",
+  });
+
+  const [tourimages, settourimages] = useState([]);
+  const [message, setMessage] = useState({ text: "", color: "" });
+  const showMessage = (text, color) => {
+    setMessage({ text, color });
+    setTimeout(() => {
+      setMessage({ text: "", color: "" });
+    }, 2500);
   };
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
+  function handleforminput(event) {
+    setTourDetail({
+      ...tourdetail,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleImageChange(event) {
+    const files = event.target.files;
+    const fileArray = Array.from(files);
+
+    if (fileArray.length > 3) {
+      showMessage("You can only upload a maximum of 3 images", "red");
+      settourimages([]);
+    } else {
+      settourimages(fileArray);
+    }
+  }
+
+  function formsubmit(event) {
+    event.preventDefault();
+    if (!tourdetail.name || !tourdetail.price || !tourdetail.description) {
+      showMessage("Please fill the complete form", "red");
+      return;
+    }
+    if (tourimages.length === 0) {
+      showMessage("Please upload at least one image", "red");
+      return;
+    }
+    const formdata = new FormData();
+    formdata.append("name", tourdetail.name);
+    formdata.append("price", tourdetail.price);
+    formdata.append("description", tourdetail.description);
+
+    tourimages.forEach((image) => {
+      formdata.append("images", image);
+    });
+
+    axios
+      .post("http://127.0.0.1:8080/api/tour/addtour", formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${guidetoken}`,
+        },
+      })
+      .then(function (response) {
+        console.log(formdata);
+        console.log(response);
+        setTourDetail({
+          name: "",
+          price: "",
+          description: "",
+        });
+        settourimages([]);
+        document.getElementById("files").value = "";
+        showMessage("Tour added successfully", "green");
+      })
+      .catch((error) => {
+        console.log(error);
+        showMessage("Failed to add tour. Please try again", "red");
+      });
+  }
+
+  const deleteTour = (tourid) => {
+    axios
+      .delete(`http://127.0.0.1:8080/api/tour/deletetour/${tourid}`, {
+        headers: {
+          Authorization: `Bearer ${guidetoken}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        showMessage("Tour deleted successfully", "green");
+        setTours((prevTours) => prevTours.filter((tour) => tour.id !== tourid));
+      })
+      .catch((error) => {
+        console.error("Failed to delete tour:", error);
+        showMessage("Failed to delete tour. Please try again", "red");
+      });
   };
+
   return (
     <div className="flex items-center justify-center mt-12">
       <div className="flex flex-col border border-gray-400 rounded w-80 ">
+        <p className="text-center" style={{ color: message.color }}>
+          {message.text}
+        </p>
         <input
           className="rounded sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh] sm:m-[0.9vh] lg:m-[0.8vh] lg:m-[0.5vh] sm:p-[1.8vw] lg:p-[0.7vw] lg:p-[0.5vw] bg-inputBoxColor outline-none focus:shadow-outline-blue focus:border-blue-500 focus:ring focus:ring-blue-400"
           placeholder="Add tour/trek name"
-          name="tour"
+          name="name"
           type="text"
+          onChange={handleforminput}
         />
         <textarea
           name="description"
           placeholder="Add tour/trek description"
+          onChange={handleforminput}
           className="w-64 border border-gray-400 rounded sm:p-[1.8vw] lg:p-[0.7vw] lg:p-[0.5vw] sm:m-[0.9vh] lg:m-[0.8vh] lg:m-[0.5vh]"
         ></textarea>
         <input
           className="rounded sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh] sm:m-[0.9vh] lg:m-[0.8vh] lg:m-[0.5vh] sm:p-[1.8vw] lg:p-[0.7vw] lg:p-[0.5vw] bg-inputBoxColor outline-none focus:shadow-outline-blue focus:border-blue-500 focus:ring focus:ring-blue-400"
           placeholder="Add tour/trek price"
-          name="tourprice"
+          name="price"
           type="text"
+          onChange={handleforminput}
         />
         <label
           htmlFor="files"
@@ -71,60 +180,32 @@ export default function Dashboard() {
           name="files"
           multiple
           className="sm:m-[0.9vh] lg:m-[0.8vh] lg:m-[0.5vh]"
+          onChange={handleImageChange}
         />
-        <button className="rounded bg-buttonColor sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh] m-[1vh] text-background shadow-2xl">
+        <button
+          className="rounded bg-buttonColor sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh] m-[1vh] text-background shadow-2xl"
+          onClick={formsubmit}
+        >
           Submit
         </button>
       </div>
       <div className="flex flex-wrap w-[60vw] mx-4 max-h-[80vh] overflow-y-auto">
-        {trekkingExpeditions.map((content, index) => (
+        {tours.map((tour, index) => (
           <div
             key={index}
             className="w-80 border border-gray-400 rounded mx-2 my-2"
           >
-            <div className="flex items-center justify-center">
-              <button onClick={prevSlide} className="text-white rounded-l">
-                <Image
-                  width={250}
-                  height={70}
-                  src="/Images/left-arrow.png"
-                  alt="left-arrow"
-                />
-              </button>
-              <div className="overflow-hidden flex-shrink-0">
-                {images.map((image, index) => (
-                  <Image
-                    width={250}
-                    height={70}
-                    key={index}
-                    src={image}
-                    alt={`Slide ${index + 1}`}
-                    className={`w-full ${
-                      index === currentIndex ? "" : "hidden"
-                    } rounded my-2`}
-                  />
-                ))}
-              </div>
-              <button onClick={nextSlide} className="text-white  rounded-r">
-                <Image
-                  width={250}
-                  height={70}
-                  src="/Images/right-arrow.png"
-                  alt="right-arrow"
-                />
-              </button>
-            </div>
-            <div className="flex justify-around">
-              <h1>Trek title</h1>
-              <p>120$</p>
+            <div className="flex justify-around mt-4">
+              <h1>Name: {tour.name}</h1>
+              <p>Price: {tour.price}</p>
             </div>
             <p className="mx-2 my-2">
               {showContent[index] ? (
-                <>{content}</>
+                <>{tour.description}</>
               ) : (
                 <>
-                  {content.substring(0, 100)}{" "}
-                  {content.length > 100 && (
+                  {tour.description.substring(0, 100)}{" "}
+                  {tour.description.length > 100 && (
                     <>
                       {" "}
                       <span
@@ -138,7 +219,10 @@ export default function Dashboard() {
                 </>
               )}
             </p>
-            <button className="rounded bg-red-600 sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh] m-[1vh] text-background shadow-2xl">
+            <button
+              className="rounded bg-red-600 sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh] m-[1vh] text-background shadow-2xl"
+              onClick={() => deleteTour(tour._id)}
+            >
               Delete
             </button>
           </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,58 @@
 "use client";
 
+import { useState } from "react";
+import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+  });
+  const [message, setMessage] = useState({ text: "", color: "" });
 
-  const handleLogin = () => {
-    router.push("/dashboard");
+  const showMessage = (text, color) => {
+    setMessage({ text, color });
+    setTimeout(() => {
+      setMessage({ text: "", color: "" });
+    }, 2500);
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = async () => {
+    try {
+      if (!formData.name || !formData.password) {
+        showMessage("Please provide valid username & password", "red");
+        return;
+      }
+      try {
+        const response = await axios.post(
+          // "https://wandermate-backend.onrender.com/api/login",
+          "http://127.0.0.1:8080/api/guide/loginguide",
+          {
+            userName: formData.name,
+            password: formData.password,
+          }
+        );
+        console.log(response, "response");
+        localStorage.setItem("Guidetoken", response.data.Token);
+        showMessage("Login successful", "green");
+        router.replace("/dashboard");
+      } catch (error) {
+        console.error("Error:", error);
+        showMessage(error.response.data.message, "red");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
@@ -24,17 +69,20 @@ export default function Home() {
         <h1 className="text-center mb-[2vh] sm:text-4xl md:text-6xl lg:text-5xl ">
           Hello guide
         </h1>
+        <p style={{ color: message.color }}>{message.text}</p>
         <input
           className="rounded sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh]  sm:m-[0.9vh] lg:m-[0.8vh] lg:m-[0.5vh] sm:p-[1.8vw] lg:p-[0.7vw] lg:p-[0.5vw] bg-inputBoxColor outline-none focus:shadow-outline-blue focus:border-blue-500 focus:ring focus:ring-blue-400"
-          placeholder="Email"
-          name="email"
+          placeholder="Username"
+          name="name"
           type="text"
+          onChange={handleInputChange}
         />
         <input
           className="rounded sm:w-[70vw] md:w-[45vw] lg:w-[15vw] sm:h-[5vh] md:h-[5vh] lg:h-[4vh]  sm:m-[0.9vh] lg:m-[0.8vh] lg:m-[0.5vh] sm:p-[1.8vw] lg:p-[0.7vw] lg:p-[0.5vw] bg-inputBoxColor outline-none focus:shadow-outline-blue focus:border-blue-500 focus:ring focus:ring-blue-400"
           placeholder="Password"
           name="password"
           type="password"
+          onChange={handleInputChange}
         />
         <button
           onClick={handleLogin}
